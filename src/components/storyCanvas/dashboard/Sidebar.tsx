b079b1faddbbb527cca24bfbf0f551c92ff3cc32
aleixmarsa <aleix.marsa@gmail.com>
2025-03-31 22:05:20 +0200

    refactor(sidebar): simplify Sidebar component by removing unused state and effects

--- src/components/storyCanvas/dashboard/Sidebar.tsx
+++ src/components/storyCanvas/dashboard/Sidebar.tsx
@@ -1,35 +1,14 @@
 "use client";
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { useCmsStore } from "@/stores/cms-store";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
 
 const Sidebar = () => {
   const router = useRouter();
-
-  const {
-    stories,
-    sections,
-    selectedStory,
-    selectedSection,
-    setStories,
-    selectSection,
-  } = useCmsStore();
-
-  useEffect(() => {
-    if (stories.length === 0) {
-      const fetchStories = async () => {
-        const res = await fetch("/api/stories");
-        const data = await res.json();
-        setStories(data);
-      };
-      fetchStories();
-    }
-  }, [stories.length, setStories]);
+  const pathname = usePathname();
 
   const handleLogout = () => {
     console.log("Logging out...");
@@ -37,58 +16,20 @@ const Sidebar = () => {
   };
 
   return (
-    <aside className="w-64 h-screen border-r bg-muted p-6 flex flex-col">
-      <Button
-        asChild
-        variant="link"
-        className="w-full justify-start pl-0 text-lg font-semibold text-left mb-6"
-        size="lg"
-      >
-        <Link href="/admin/dashboard">Story Canvas</Link>
-      </Button>
+    <aside className="w-64 h-screen bg-muted p-6 flex flex-col">
+      <h1 className="w-full justify-start pl-0 text-lg font-semibold text-left mb-6">
+        Story Canvas
+      </h1>
       <ScrollArea className="grow pr-2">
-        <ul className="space-y-4">
-          {stories.map((story) => (
-            <li key={story.id}>
-              <Button
-                asChild
-                variant="link"
-                className={`w-full justify-start text-left ${
-                  selectedStory?.id === story.id ? "underline" : ""
-                }`}
-                size="sm"
-              >
-                <Link href={`/admin/dashboard/${story.slug}`}>
-                  {story.title}
-                </Link>
-              </Button>
-
-              {selectedStory?.id === story.id && sections.length > 0 && (
-                <ul className="mt-2 pl-4 space-y-1">
-                  {sections.map((section) => (
-                    <li key={section.id}>
-                      <Button
-                        variant="link"
-                        size="sm"
-                        className={`w-full justify-start text-left pl-0" ${
-                          selectedSection?.id === story.id ? "underline" : ""
-                        }`}
-                        onClick={() => selectSection(section)}
-                        asChild
-                      >
-                        <Link
-                          href={`/admin/dashboard/${story.slug}/${section.slug}`}
-                        >
-                          {section.name}
-                        </Link>
-                      </Button>
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </li>
-          ))}
-        </ul>
+        <Button
+          asChild
+          variant="ghost"
+          className={`w-full justify-start text-left ${
+            pathname === "/admin/dashboard" ? " bg-white  " : ""
+          }`}
+        >
+          <Link href="/admin/dashboard">Dashboard</Link>
+        </Button>
       </ScrollArea>
       <div className="mt-6 pt-4 border-t">
         <Button
