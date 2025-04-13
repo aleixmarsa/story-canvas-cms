cf352a49e450feb7fa6f5ccac4f3e3463b69c9b3
aleixmarsa <aleix.marsa@gmail.com>
2025-04-13 15:45:36 +0200

    test(dep): Add necessary dependencies and scripts

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,18 @@
 import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
 
 const config: Config = {
   preset: "ts-jest",
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
   testMatch: ["**/__tests__/**/*.test.ts"],
   moduleFileExtensions: ["ts", "js"],
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/$1",
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
   globals: {
     "ts-jest": {
@@ -15,4 +21,5 @@ const config: Config = {
   },
 };
 
-export default config;
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(config);
