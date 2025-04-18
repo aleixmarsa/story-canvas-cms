d65c566f31e0a0d384092bb58170cfdc52c2ad85
aleixmarsa <aleix.marsa@gmail.com>
2025-04-18 18:03:12 +0200

    refactor(user): streamline user deletion process and add toast notifications

--- src/components/storyCanvas/dashboard/user/UsersTableWrapper.tsx
+++ src/components/storyCanvas/dashboard/user/UsersTableWrapper.tsx
@@ -1,30 +1,54 @@
 "use client";
 
+import { useEffect } from "react";
 import { deleteUser } from "@/lib/actions/users/delete-user";
 import { columns } from "@/components/storyCanvas/dashboard/DataTable/UserDataTableColumns";
 import DataTable from "@/components/storyCanvas/dashboard/DataTable/DataTable";
 import { DeleteUserDialog } from "./DeleteUserDialog";
 import { CurrentUser } from "@/types/auth";
 import { useDashboardStore } from "@/stores/dashboard-store";
-import { useEffect } from "react";
+import { toast } from "sonner";
 
 export function UsersTableWrapper({ users }: { users: CurrentUser[] }) {
-  const zustandUsers = useDashboardStore((s) => s.users);
-  const setUsers = useDashboardStore((s) => s.setUsers);
-  const deleteUserFromStore = useDashboardStore((s) => s.deleteUser);
+  const {
+    users: zustandUsers,
+    setUsers,
+    deleteUser: deleteUserFromStore,
+    addUser,
+  } = useDashboardStore();
 
   useEffect(() => {
     setUsers(users);
   }, [users, setUsers]);
 
   const handleDelete = async (user: CurrentUser) => {
-    const res = await deleteUser(user.id);
+    // const res = await deleteUser(user.id);
 
-    if (res.success) {
-      deleteUserFromStore(user.id);
-    } else {
-      console.error("Error deleting user:", res.error);
-    }
+    //Delete user from the store
+    deleteUserFromStore(user.id);
+
+    toast.success("User has been removed", {
+      description: `${user.email} has been removed.`,
+      action: {
+        label: "Undo",
+        onClick: () => {
+          // Add the user back to the store
+          addUser(user);
+          toast.dismiss();
+          toast.success("User has been restored", {
+            description: `${user.email} has been restored.`,
+          });
+        },
+      },
+      onAutoClose: async () => {
+        // Delete user from the database when the toast is closed
+        const res = await deleteUser(user.id);
+        if (!res.success) {
+          toast.error("Failed to delete user");
+          addUser(user);
+        }
+      },
+    });
   };
 
   return (
