1820b368f0894332352adc16ff99fa04ac3c75e7
aleixmarsa <aleix.marsa@gmail.com>
2025-04-19 20:16:12 +0200

    feat(db): update seed script to use hashed password

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+
 const prisma = new PrismaClient();
 async function main() {
+  const hashedPassword = await bcrypt.hash("securepassword", 10);
   await prisma.user.create({
     data: {
-      email: "admin@test.com",
-      password: "testpassword",
+      email: "admin@cms.com",
+      password: hashedPassword,
       role: "ADMIN",
     },
   });
