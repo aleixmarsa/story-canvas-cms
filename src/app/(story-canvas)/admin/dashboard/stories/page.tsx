346095e6137ead69fbeb69e1bb8ffb36a0897a4f
aleixmarsa <aleix.marsa@gmail.com>
2025-04-20 18:12:54 +0200

    fix(import): correct import for StoryTableWrapper component

--- src/app/(story-canvas)/admin/dashboard/stories/page.tsx
+++ src/app/(story-canvas)/admin/dashboard/stories/page.tsx
@@ -1,7 +1,7 @@
 import DashboardHeader from "@/components/storyCanvas/dashboard/DashboardHeader";
 import { ROUTES } from "@/lib/constants/storyCanvas";
 import { getCurrentUser } from "@/lib/dal/auth";
-import { StoryTableWrapper } from "@/components/storyCanvas/dashboard/story/StoryTableWrapper";
+import StoryTableWrapper from "@/components/storyCanvas/dashboard/story/StoryTableWrapper";
 import { redirect } from "next/navigation";
 
 const StoriesPage = async () => {
