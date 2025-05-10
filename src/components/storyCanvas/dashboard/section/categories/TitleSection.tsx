b58df367097f072a39a2bf3d54c7ae1a4435e7d0
aleixmarsa <aleix.marsa@gmail.com>
2025-05-10 21:05:37 +0200

    refactor(TitleSection): integrate text animation and scroll trigger functionality

--- src/components/storyCanvas/dashboard/section/categories/TitleSection.tsx
+++ src/components/storyCanvas/dashboard/section/categories/TitleSection.tsx
@@ -1,6 +1,51 @@
+"use client";
+
+import { useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { TitleSectionProps } from "@/sections/validation/sections/title-section-schema";
 import RichTextContent from "./fields/RichTextContent";
-const TitleSection = ({ text, textPadding }: TitleSectionProps) => {
+import { useGSAP } from "@gsap/react";
+import { applyAnimation } from "@/lib/animations/apply-animation";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const TitleSection = ({
+  text,
+  textPadding,
+  textAnimation,
+  scrollTrigger,
+}: TitleSectionProps) => {
+  const textRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(
+    () => {
+      const textElement = textRef.current;
+      if (
+        !textElement ||
+        !textAnimation ||
+        textAnimation?.animationType === "none"
+      )
+        return;
+
+      const scroll =
+        scrollTrigger && scrollTrigger.start !== "none" ? scrollTrigger : null;
+
+      const timeline = gsap.timeline({
+        scrollTrigger: scroll
+          ? {
+              trigger: textElement,
+              start: scroll.start,
+              end: "scroll.end",
+              scrub: scroll.scrub === "true",
+            }
+          : undefined,
+      });
+
+      applyAnimation(textElement, textAnimation, timeline);
+    },
+    { dependencies: [textAnimation, scrollTrigger], revertOnUpdate: true }
+  );
   const inlineStyle: React.CSSProperties = {
     paddingTop: textPadding?.top ? `${textPadding.top}px` : undefined,
     paddingBottom: textPadding?.bottom ? `${textPadding.bottom}px` : undefined,
@@ -9,7 +54,9 @@ const TitleSection = ({ text, textPadding }: TitleSectionProps) => {
   };
   return (
     <div className={`py-8 `} style={inlineStyle}>
-      <RichTextContent html={text} />
+      <div ref={textRef}>
+        <RichTextContent html={text} />
+      </div>
     </div>
   );
 };
