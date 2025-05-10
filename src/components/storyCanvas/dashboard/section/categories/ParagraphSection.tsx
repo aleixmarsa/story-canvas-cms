00f2dfe8f19d658e9439d548d8159ce3d986217a
aleixmarsa <aleix.marsa@gmail.com>
2025-05-10 12:59:38 +0200

    refactor(paragraph-section): add textPadding prop

--- src/components/storyCanvas/dashboard/section/categories/ParagraphSection.tsx
+++ src/components/storyCanvas/dashboard/section/categories/ParagraphSection.tsx
@@ -1,9 +1,16 @@
 import { ParagraphSectionProps } from "@/sections/validation/sections/paragraph-section-schema";
 import RichTextContent from "./fields/RichTextContent";
 
-const ParagraphSection = ({ body }: ParagraphSectionProps) => {
+const ParagraphSection = ({ body, textPadding }: ParagraphSectionProps) => {
+  const inlineStyle: React.CSSProperties = {
+    paddingTop: textPadding?.top ? `${textPadding.top}px` : undefined,
+    paddingBottom: textPadding?.bottom ? `${textPadding.bottom}px` : undefined,
+    paddingLeft: textPadding?.left ? `${textPadding.left}px` : undefined,
+    paddingRight: textPadding?.right ? `${textPadding.right}px` : undefined,
+  };
+
   return (
-    <div className="py-8">
+    <div className="py-8" style={inlineStyle}>
       <RichTextContent html={body} />
     </div>
   );
