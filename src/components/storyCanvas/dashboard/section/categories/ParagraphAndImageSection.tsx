a0bf243baa285f012ab119b628d752134d05e813
aleixmarsa <aleix.marsa@gmail.com>
2025-05-10 21:13:04 +0200

    refactor(ParagraphSection): integrate text animation and scroll trigger functionality

--- src/components/storyCanvas/dashboard/section/categories/ParagraphAndImageSection.tsx
+++ src/components/storyCanvas/dashboard/section/categories/ParagraphAndImageSection.tsx
@@ -1,5 +1,14 @@
+"use client";
+
+import { useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ParagraphAndImageSectionProps } from "@/sections/validation/sections/paragraph-and-image-section-schema";
 import RichTextContent from "./fields/RichTextContent";
+import { useGSAP } from "@gsap/react";
+import { applyAnimation } from "@/lib/animations/apply-animation";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const ParagraphAndImageSection = ({
   body,
@@ -8,8 +17,46 @@ const ParagraphAndImageSection = ({
   caption,
   layout,
   imageSize,
+  textAnimation,
+  imageAnimation,
+  scrollTrigger,
 }: ParagraphAndImageSectionProps) => {
   const isLeft = layout === "left";
+
+  const textRef = useRef<HTMLDivElement>(null);
+  const imageRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(
+    () => {
+      const textElement = textRef.current;
+      const imageElement = imageRef.current;
+      if (
+        !textElement ||
+        !imageElement ||
+        !textAnimation ||
+        textAnimation?.animationType === "none"
+      )
+        return;
+
+      const scroll =
+        scrollTrigger && scrollTrigger.start !== "none" ? scrollTrigger : null;
+
+      const timeline = gsap.timeline({
+        scrollTrigger: scroll
+          ? {
+              trigger: textElement,
+              start: scroll.start,
+              end: "scroll.end",
+              scrub: scroll.scrub === "true",
+            }
+          : undefined,
+      });
+
+      applyAnimation(textElement, textAnimation, timeline);
+      applyAnimation(imageElement, imageAnimation, timeline);
+    },
+    { dependencies: [textAnimation, scrollTrigger], revertOnUpdate: true }
+  );
 
   return (
     <div className="py-8">
@@ -18,7 +65,7 @@ const ParagraphAndImageSection = ({
           isLeft ? "md:flex-row-reverse" : ""
         } gap-6 items-center`}
       >
-        <div className="shrink-0 w-full md:w-1/2">
+        <div className="shrink-0 w-full md:w-1/2" ref={imageRef}>
           {image ? (
             <img
               src={image.url}
@@ -32,8 +79,9 @@ const ParagraphAndImageSection = ({
           )}
           {caption && <RichTextContent html={caption} />}
         </div>
-
-        <RichTextContent html={body} />
+        <div ref={textRef}>
+          <RichTextContent html={body} />
+        </div>
       </div>
     </div>
   );
