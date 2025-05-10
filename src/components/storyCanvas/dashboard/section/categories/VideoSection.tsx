20a20da0f6ee1f5e45ebd30ab45bb42af7d43c3d
aleixmarsa <aleix.marsa@gmail.com>
2025-05-10 21:28:10 +0200

    fix(VideoSection): render video only when video exists

--- src/components/storyCanvas/dashboard/section/categories/VideoSection.tsx
+++ src/components/storyCanvas/dashboard/section/categories/VideoSection.tsx
@@ -4,18 +4,25 @@ import RichTextContent from "./fields/RichTextContent";
 const VideoSection = ({ video, title }: VideoSectionProps) => {
   return (
     <div className="py-8 text-center">
-      <video
-        className="mx-auto rounded-lg shadow-md"
-        controls
-        src={video.url}
-        poster={video.url}
-        width="600"
-        height="400"
-        preload="metadata"
-        onError={(e) => {
-          console.error("Video loading error:", e);
-        }}
-      />
+      <div className="mb-4">
+        {video ? (
+          <video
+            className="mx-auto rounded-lg shadow-md"
+            controls
+            src={video.url}
+            poster={video.url}
+            width="600"
+            height="400"
+            preload="metadata"
+            onError={(e) => {
+              console.error("Video loading error:", e);
+            }}
+          />
+        ) : (
+          <></>
+        )}
+      </div>
+
       <RichTextContent html={title} />
     </div>
   );
