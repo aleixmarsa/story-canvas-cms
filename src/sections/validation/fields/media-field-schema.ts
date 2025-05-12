f3f0aa5cd2b83729114455d0c54e4fbe473f1bfa
aleixmarsa <aleix.marsa@gmail.com>
2025-05-12 22:52:15 +0200

    fix(validation): refactor mediaFieldSchema to use object instead of union

--- src/sections/validation/fields/media-field-schema.ts
+++ src/sections/validation/fields/media-field-schema.ts
@@ -1,11 +1,15 @@
 import { z } from "zod";
 
-export const mediaFieldSchema = z.union([
-  z.object({
-    url: z.string().url("Invalid image URL"),
-    publicId: z.string(),
-  }),
-  z.string().url("Invalid image URL"),
+export const mediaFieldSchema = z.object({
+  url: z.string().url("Invalid image URL"),
+  publicId: z.string(),
+});
+
+export const mediaFieldSchemaOrString = z.union([
+  mediaFieldSchema,
+  z.string().url(),
 ]);
 
 export type MediaField = z.infer<typeof mediaFieldSchema>;
+
+export type MediaFieldOrString = z.infer<typeof mediaFieldSchemaOrString>;
