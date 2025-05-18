b02f6042ce7ce037c1e18e4999f62d55bcf62e5d
aleixmarsa <aleix.marsa@gmail.com>
2025-05-18 10:50:13 +0200

    fix(docusaurus.config): update site URL and organization/project names

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -9,15 +9,15 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://story-canvas-cms.vercel.app",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "aleixmarsa", // Usually your GitHub org/user name.
+  projectName: "story-canvas-cms", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
@@ -52,7 +52,7 @@ const config: Config = {
     navbar: {
       title: "StoryCanvas Docs",
       logo: {
-        alt: "StoryCanvas Lzogo",
+        alt: "StoryCanvas Logo",
         src: "img/story-canvas-logo.svg",
       },
       items: [
