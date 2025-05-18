6dd1c59b67b573fa40b4c4a13461ef01822fcb2b
aleixmarsa <aleix.marsa@gmail.com>
2025-05-18 11:48:43 +0200

    fix(CreateInitialUserPage): change redirect for conditional rendering and improved messaging

--- src/app/(story-canvas)/admin/layout.tsx
+++ src/app/(story-canvas)/admin/layout.tsx
@@ -2,7 +2,6 @@ import { redirect } from "next/navigation";
 import { headers } from "next/headers";
 import { ROUTES } from "@/lib/constants/story-canvas";
 import { Toaster } from "@/components/ui/sonner";
-import { countAllUsers } from "@/lib/dal/users";
 import { checkDbConnection } from "@/lib/db";
 
 export default async function AdminLayout({
@@ -26,35 +25,9 @@ export default async function AdminLayout({
     redirect(`${ROUTES.error}?type=db`);
   }
 
-  // Fetch the number of users
-  const countResult = await countAllUsers();
-  if ("error" in countResult && !isErrorPage) {
-    redirect(`${ROUTES.error}?type=count`);
-  }
-
   // Redirect to dashboard if user is logged in and on initial user page or login page
   if (isLoggedIn && (isInitialUserPage || isLoginPage)) {
     redirect(ROUTES.dashboard);
-  }
-
-  // Redirect to create initial user page if no users exist and not on initial user page
-  if (
-    !isLoggedIn &&
-    "numberOfUsers" in countResult &&
-    countResult.numberOfUsers === 0 &&
-    !isInitialUserPage
-  ) {
-    redirect(ROUTES.createInitalUser);
-  }
-
-  // Redirect to login page if a user exists and not on login page
-  if (
-    !isLoggedIn &&
-    "numberOfUsers" in countResult &&
-    countResult.numberOfUsers > 0 &&
-    !isLoginPage
-  ) {
-    redirect(ROUTES.login);
   }
 
   return (
